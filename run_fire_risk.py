cb2036f945ee6e60d02a0ba1de6a6485e32cc975
caslasgee <98241971+caslasgee@users.noreply.github.com>
2026-03-11 14:29:53 +0600

    Add files via upload

--- run_fire_risk.py
+++ run_fire_risk.py
@@ -1,4 +1,6 @@
+import os
 from fire_risk.app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run_server(host="0.0.0.0", port=port, debug=False)
